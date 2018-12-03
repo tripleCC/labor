d80796a1a08ee9655b65e2c6053190119c9fc775
tripleCC <triplec.linux@gmail.com>
2018-12-03 11:04:28 +0800

    更改tag pl 创建重试延时

--- lib/labor/deploy_service/process_pod.rb
+++ lib/labor/deploy_service/process_pod.rb
@@ -63,11 +63,16 @@ module Labor
 				# 规避了这个问题
 				pipeline = gitlab.newest_active_pipeline(project.id, name)
 				# 这里立刻创建 pipeline 的话，可能会出现找不到 tag 错误，所以延迟重试 0.15
-				retry_rescue Gitlab::Error::BadRequest, 5, 1 do |rest_times|
-					logger.info("pod deploy (id: #{deploy.id}, name: #{deploy.name}): create pipeline with rest times #{rest_times}")
-					pipeline = gitlab.create_pipeline(project.id, name)	if pipeline.nil?
+				unless pipeline
+					retry_rescue Gitlab::Error::BadRequest, 5, 0.5 do |rest_times|
+						logger.info("pod deploy (id: #{deploy.id}, name: #{deploy.name}): create pipeline with rest times #{rest_times}")
+						pipeline = gitlab.create_pipeline(project.id, name)	
+					end
+					logger.info("pod deploy (id: #{deploy.id}, name: #{deploy.name}): run pipeline (#{pipeline.id})")
+				else
+					logger.info("pod deploy (id: #{deploy.id}, name: #{deploy.name}): newest active pipeline (#{pipeline.id})")
 				end
-				logger.info("pod deploy (id: #{deploy.id}, name: #{deploy.name}): run pipeline (#{pipeline.id})")
+
 				pipeline
 			end
 		end
