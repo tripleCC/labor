1d5f8fcd14718b192e6a86b20ee99a13016402c2
tripleCC <triplec.linux@gmail.com>
2018-12-24 19:18:05 +0800

    feat: 只创建最新版本的podspec好了

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,22 @@ require 'benchmark'
 
 source = Pod::Config.instance.sources_manager.default_source
 
-puts "start create specifications (last 2)"
+puts "start create specifications (last 1)"
 benchmark = Benchmark.measure {
 	Parallel.each(source.pods, in_processes: 8) do |pod|
 		versions = source.versions(pod)
-			versions.sort.last(2).each do |version|
-				# Specification.from_string 会 chdir ！
-				spec = source.specification(pod, version)
-				spec_path = source.specification_path(pod, version)
-				spec_content = File.read(spec_path)
-				# p "create or update specification #{pod} #{version}"
-				@reconnected ||= Labor::Specification.connection.reconnect! || true
-    		Labor::Specification.create_or_update_specification_by(pod, version.to_s, spec_content, spec)
-			end
+		versions.sort.last.tap do |version|
+			# Specification.from_string 会 chdir ！
+			spec = source.specification(pod, version)
+			spec_path = source.specification_path(pod, version)
+			spec_content = File.read(spec_path)
+			# p "create or update specification #{pod} #{version}"
+			@reconnected ||= Labor::Specification.connection.reconnect! || true
+  		Labor::Specification.create_or_update_specification_by(pod, version.to_s, spec_content, spec)
+		end
 	end
 }
-puts "finish create specifications (last 2) with benchmark #{benchmark}"
+puts "finish create specifications (last 1) with benchmark #{benchmark}"
 # Labor::Specification.transaction do
 # 	source.pods.each do |pod|
 # 		versions = source.versions(pod)
