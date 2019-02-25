0bb10759cf8fabb5362c28525f7be98b547c1b69
tripleCC <triplec.linux@gmail.com>
2019-02-25 17:19:25 +0800

    feat: DEPLOY_JOBS add publish_code_pod publish_binary_pod

--- lib/labor/remote_file/gitlab_ci_yaml.rb
+++ lib/labor/remote_file/gitlab_ci_yaml.rb
@@ -8,7 +8,7 @@ module Labor
 			CI_CONFIG_FILE_NAME = '.gitlab-ci.yml'.freeze
 
 			# DEPLOY_JOBS = ['package_framework', 'publish_pod'].freeze
-			DEPLOY_JOBS = ['publish_pod'].freeze
+			DEPLOY_JOBS = ['publish_pod', 'publish_code_pod', 'publish_binary_pod'].freeze
 
 			def initialize(project_id, ref = 'master', path = nil)
 				path ||= gitlab.file_path(project_id, CI_CONFIG_FILE_NAME, ref, 1)
